afb86c88df79176a24a9b601d518b30b2158a50a
bazzl1202 <71136617+bazzl1202@users.noreply.github.com>
2020-10-05 09:56:41 -0400

    Now determines if a number is prime or not

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Deteremines if a number is prime or not
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to enter an integer
+    System.out.println("Which number would you like to check?");
+    int integer = input.nextInt();
+
+    // declare and initialize a counting variable
+    int count = 1;
+
+    while(count <= integer){
+      count = count + 1;
+      if(integer%count == 0 && count < integer){
+        System.out.println(integer + " is not a prime number");
+        break;
+      } else if(count == integer && integer%integer == 0){
+        System.out.println( integer + " is a prime number");
+      }
+    } 
+      
     
   }
 }
+
